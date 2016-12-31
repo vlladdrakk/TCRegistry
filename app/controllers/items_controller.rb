3cb624eb45440b279090a69e9abcebab7e3b0912
vlladdrakk <vlladdrakk@gmail.com>
2016-12-30 22:05:11 -0400

    added get and update controllers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,31 @@
 class ItemsController < ApplicationController
   def create
-  	# get the item information from the parameters
-  	item_name = params[:name]
-  	needed = params[:needed]
-  	image = params[:image]
-  	description = params[:description]
+    # get the item information from the parameters
+    item_name = params[:name]
+    needed = params[:needed]
+    image = params[:image]
+    description = params[:description]
 
-  	newItem = RegistryItem.new(name: item_name,needed: needed, pledged: 0, picture: image, description: description)
-  	newItem.save
+    newItem = RegistryItem.new(name: item_name,needed: needed, pledged: 0, picture: image, description: description)
+    newItem.save
 
-  	# redirect to management interface
-  	redirect_to action: "manage"
+    # redirect to management interface
+    redirect_to action: "manage"
   end
 
   def delete
-  	id = params[:id]
-	item = RegistryItem.find(id)
-	item.delete()
-	item.save()
+    id = params[:id]
+    item = RegistryItem.find(id)
+    item.delete()
+    item.save()
   end
 
+  def get id
+    item = RegistryItem.find(id)
+    render json: item.as_json
+  end  
+
   def update
-  	
+    
   end
 end
