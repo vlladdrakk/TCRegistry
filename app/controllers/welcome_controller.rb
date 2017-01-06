545f0e8be371419ba2c3d546d8af04b66fff3063
vlladdrakk <vlladdrakk@gmail.com>
2017-01-05 21:39:01 -0400

    Added functionality for displaying only the items from specific categories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,16 @@
 class WelcomeController < ApplicationController
   def index
-  	#@selectedCategory = nil
+  	selectedCategory = params[:selection] 
   	@categories = Category.all
-  	@items = RegistryItem.all #.where(category: @selectedCategory)
+  	if selectedCategory.nil?
+  		@items = RegistryItem.where(category_id: 1)
+  	else
+  		selected_id = Category.where(name: selectedCategory)[0]
+  		pp selected_id
+  		selected_id = selected_id.id
+	  	@items = RegistryItem.where(category_id: selected_id)
+	  	p "items"
+	  	pp @items
+	end
   end
 end
