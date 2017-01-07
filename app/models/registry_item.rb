bb7d817897ba138c5b4fe7309d2810f690be303e
vlladdrakk <vlladdrakk@gmail.com>
2017-01-06 23:58:23 -0400

    Added functions for getting the picture url and file (these may not be needed)

--- app/models/registry_item.rb
+++ app/models/registry_item.rb
@@ -5,4 +5,12 @@ class RegistryItem < ApplicationRecord
 	:styles => {
       :thumb => "250x250#"}
   	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+  	def get_file
+		return File.open(picture.path,"r")
+  	end
+
+  	def get_url
+  		return picture.url
+  	end
 end
