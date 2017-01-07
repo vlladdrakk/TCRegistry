e3a520993ff3a2a243c05e639e1dd2f56339dc6a
vlladdrakk <vlladdrakk@gmail.com>
2017-01-07 12:47:24 -0400

    removed remenents of template css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( shop-homepage.css )
 Rails.application.config.assets.precompile += %w( jquery.js )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( content.js )
