c831a84b93d446731f6a7c95dd6d9574ce0c61f5
vlladdrakk <vlladdrakk@gmail.com>
2017-02-11 15:25:20 -0400

    Created colors model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106011400) do
+ActiveRecord::Schema.define(version: 20170211171048) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "colors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "hex_value"
+    t.integer  "type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "registry_items", force: :cascade do |t|
