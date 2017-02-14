5667ae379e6bd16e7b8fffb4536f42bfae0eee4a
vlladdrakk <vlladdrakk@gmail.com>
2017-02-13 22:05:30 -0400

    Updated category ui

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   	selectedCategory = params[:selection]
   	@categories = Category.all
   	if selectedCategory.nil?
-  		@items = RegistryItem.where(category_id: 1)
+  		@items = RegistryItem.where(category_id: Category.first.id)
   	else
   		selected_id = Category.find(selectedCategory).id
 	  	@items = RegistryItem.where(category_id: selected_id)
