66ca7a77ec877a900772d271cafabc22e8a6c74c
vlladdrakk <vlladdrakk@gmail.com>
2017-02-23 22:38:52 -0400

    Updated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Color.create(
-  name: "Primary",
-  hex_value: "bfbff2",
-  type_id: 1
-)
-
-Color.create(
-  name: "Secondary",
-  hex_value: "ffd6b3",
-  type_id: 2
+AccessCode.create(
+  code: 314159
 )
