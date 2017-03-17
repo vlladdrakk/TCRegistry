a14a396007b9f2a34a4b81426dffd060afdde263
vlladdrakk <vlladdrakk@gmail.com>
2017-03-16 23:06:50 -0300

    Don't send request if cancelled

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -7,30 +7,33 @@ function claimItem(element) {
 
   // prompt user for the access code
   var access_code = prompt("Please enter the access code that is in the bottom left corner of your invitation\n(This action cannot be undone)");
-  $.ajax({
-    url: "/items/claim",
-    method: "POST",
-    data: {
-      id: item_id,
-      code: access_code
-    },
-    success: function(response) {
-      if (response.code !== "500") {
-        if (response.needed === 0) {
-          $("#item-" + item_id).addClass("hidden");
+
+  if (access_code !== null) {
+    $.ajax({
+      url: "/items/claim",
+      method: "POST",
+      data: {
+        id: item_id,
+        code: access_code
+      },
+      success: function(response) {
+        if (response.code !== "500") {
+          if (response.needed === 0) {
+            $("#item-" + item_id).addClass("hidden");
+          } else {
+            $("#needed-" + item_id).html(response.needed);
+          }
         } else {
-          $("#needed-" + item_id).html(response.needed);
+          if (response.result === "Invalid Code") {
+            alert("Incorrect access code,\n\nPlease try again or contact the soon-to-be-wed couple <3")
+          } else {
+            alert("An unexpected error occured...Now locating a repair monkey");
+          }
         }
-      } else {
-        if (response.result === "Invalid Code") {
-          alert("Incorrect access code,\n\nPlease try again or contact the soon-to-be-wed couple <3")
-        } else {
-          alert("An unexpected error occured...Now locating a repair monkey");
-        }
+      },
+      error: function(error) {
+        console.log(error);
       }
-    },
-    error: function(error) {
-      console.log(error);
-    }
-  })
+    })
+  }
 }
